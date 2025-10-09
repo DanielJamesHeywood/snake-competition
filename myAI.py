31fb0d466fe9cecd654ce78deb4ea835e5626323
Daniel James Heywood <danieljamesheywood@icloud.com>
2025-10-09 16:37:03 +0100

    Simplified possibleStates declaration.

--- myAI.py
+++ myAI.py
@@ -6,7 +6,7 @@ from examples.smartAI import smartAI as enemyAI
 
 
 def myAI(state: GameState) -> Turn:
-    possibleStates = {Turn.LEFT: {moveSnake(state, Turn.LEFT)}, Turn.STRAIGHT: {moveSnake(state, Turn.STRAIGHT)}, Turn.RIGHT: {moveSnake(state, Turn.RIGHT)}}
+    possibleStates = {turn: moveSnake(state, turn) for turn in Turn}
     return Turn.STRAIGHT
 
 def moveSnake(state: GameState, turn: Turn) -> GameState:
