455d2641846db73f61e417943859732d2ca09f5c
Daniel James Heywood <danieljamesheywood@icloud.com>
2025-10-09 15:36:38 +0100

    Documented approach.

--- myAI.py
+++ myAI.py
@@ -10,3 +10,9 @@ def myAI(state: GameState) -> Turn:
     straightStates = set()
     rightStates = set()
     return Turn.STRAIGHT
+
+# Maintain set of states achievable by immediatly turning left, right and straight.
+# Iteratively evaluate all positions in each set in turn.
+# Data structures are sets to prevent evaluating identical states twice.
+# Before adding a state to straightStates we should check that state isn't present in leftStates.
+# Before adding a state to rightStates we should check that state isn't present in straightStates or leftStates.
