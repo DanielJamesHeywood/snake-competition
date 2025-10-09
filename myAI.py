880de6a5838035b1ef2ad1ec0f71d6808b7d0590
Daniel James Heywood <danieljamesheywood@icloud.com>
2025-10-09 14:18:58 +0100

    Cleaned up myAI to allow for future reimplementation.

--- myAI.py
+++ myAI.py
@@ -2,6 +2,8 @@ import random
 from collections import deque
 from snake.logic import GameState, Turn, Snake, Direction
 
+from examples.smartAI import smartAI as enemyAI
+
 """
 Your mission, should you choose to accept it, is to write the most cracked snake AI possible.
 
@@ -13,29 +15,4 @@ Below is all of the data you'll need, and some small examples that you can uncom
 
 
 def myAI(state: GameState) -> Turn:
-    safe = []
-    enemy_bodies = set()
-    for snake in state.enemies:
-        enemy_bodies |= snake.body_set
-    for turn in list(Turn):
-        head = state.snake.get_next_head(turn)
-        if (
-            0 <= head[0] < state.width
-            and 0 <= head[1] < state.height
-            and head not in state.walls
-            and head not in state.snake.body
-            and head not in enemy_bodies
-        ):
-            safe.append(turn)
-    if not safe:
-        return Turn.STRAIGHT
-    if state.food:
-        food = list(state.food)[0]
-        current = state.snake.head
-        for turn in safe:
-            new = state.snake.get_next_head(turn)
-            if abs(food[0] - new[0]) < abs(food[0] - current[0]) or abs(
-                food[1] - new[1]
-            ) < abs(food[1] - current[1]):
-                return turn
-    return random.choice(safe)
+    return Turn.STRAIGHT
