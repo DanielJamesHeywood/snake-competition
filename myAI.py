99e072430b18b644dbf7545ba478570c0618b2e3
Daniel James Heywood <danieljamesheywood@icloud.com>
2025-10-09 16:38:38 +0100

    Fixed indentation.

--- myAI.py
+++ myAI.py
@@ -9,5 +9,6 @@ def myAI(state: GameState) -> Turn:
     possibleStates = {turn: moveSnake(state, turn) for turn in Turn}
     return Turn.STRAIGHT
 
+
 def moveSnake(state: GameState, turn: Turn) -> GameState:
     return state
