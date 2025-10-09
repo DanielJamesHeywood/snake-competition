f651830a1a3425f8ad5973170b2c1fdaedd35209
Daniel James Heywood <danieljamesheywood@icloud.com>
2025-10-09 14:19:44 +0100

    Further cleaning.

--- myAI.py
+++ myAI.py
@@ -4,15 +4,6 @@ from snake.logic import GameState, Turn, Snake, Direction
 
 from examples.smartAI import smartAI as enemyAI
 
-"""
-Your mission, should you choose to accept it, is to write the most cracked snake AI possible.
-
-All the info you'll need to do this is in the GameState and Snake classes in snake/logic.py
-
-Below is all of the data you'll need, and some small examples that you can uncomment and use if you want :)
-
-"""
-
 
 def myAI(state: GameState) -> Turn:
     return Turn.STRAIGHT
