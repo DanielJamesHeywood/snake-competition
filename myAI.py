5a7a7d24c406474aced1fc6eda840a9e9f8b91ee
Daniel James Heywood <danieljamesheywood@icloud.com>
2025-10-13 18:46:12 +0100

    Refactored newDistance out of myAI.

--- myAI.py
+++ myAI.py
@@ -29,12 +29,13 @@ def myAI(state: GameState) -> Turn:
             insert(queue, (newState, turn, turn, 1, minimumDistancesToNearestFood[newState.snake.head] + 1))
     while queue:
         state, firstTurn, lastTurn, distance, minimumDistanceToNearestFood = queue.popleft()
+        newDistance = distance + 1
         for turn in Turn:
             newState = state if turn == Turn.RIGHT else copyGameState(state)
             if moveSnake(newState, turn):
                 if newState.score > state.score:
                     return firstTurn
-                insert(queue, (newState, firstTurn, turn, distance + 1, minimumDistancesToNearestFood[newState.snake.head] + distance + 1))
+                insert(queue, (newState, firstTurn, turn, newDistance, minimumDistancesToNearestFood[newState.snake.head] + newDistance))
     return Turn.STRAIGHT
 
 
