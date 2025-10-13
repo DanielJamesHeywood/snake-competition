2f320290b2d96b0aea4056c62c1296857ae4d651
Daniel James Heywood <danieljamesheywood@icloud.com>
2025-10-13 12:30:18 +0100

    Standardised variable and function names.

--- myAI.py
+++ myAI.py
@@ -6,19 +6,19 @@ from examples.smartAI import smartAI as enemyAI
 def myAI(state: GameState) -> Turn:
     queue = deque()
     for turn in Turn:
-        new_state = copyGameState(state)
-        if move_snake(new_state, turn):
-            if new_state.score > state.score:
+        newState = copyGameState(state)
+        if moveSnake(newState, turn):
+            if newState.score > state.score:
                 return turn
-            queue.append((new_state, turn))
+            queue.append((newState, turn))
     while queue:
         state, turn = queue.popleft()
-        for new_turn in Turn:
-            new_state = copyGameState(state)
-            if move_snake(new_state, new_turn):
-                if new_state.score > state.score:
+        for newTurn in Turn:
+            newState = copyGameState(state)
+            if moveSnake(newState, newTurn):
+                if newState.score > state.score:
                     return turn
-                queue.append((new_state, turn))
+                queue.append((newState, turn))
     return Turn.STRAIGHT
 
 
@@ -26,15 +26,15 @@ def copyGameState(state: GameState) -> GameState:
     return GameState(
         width = state.width,
         height = state.height,
-        snake = copy_snake(state.snake),
-        enemies = [copy_snake(enemy) for enemy in state.enemies],
+        snake = copySnake(state.snake),
+        enemies = [copySnake(enemy) for enemy in state.enemies],
         food = state.food.copy(),
         walls = state.walls.copy(),
         score = state.score
     )
 
 
-def copy_snake(snake: Snake) -> Snake:
+def copySnake(snake: Snake) -> Snake:
     copy = Snake(0, 0, snake.id)
     copy.score = snake.score
     copy.isAlive = snake.isAlive
@@ -43,32 +43,32 @@ def copy_snake(snake: Snake) -> Snake:
     return copy
 
 
-def move_snake(state: GameState, turn: Turn) -> bool:
-    state.snake.isAlive = _move_snake(state, state.snake, turn)
+def moveSnake(state: GameState, turn: Turn) -> bool:
+    state.snake.isAlive = moveSnake(state, state.snake, turn)
     if state.snake.isAlive:
         for index in range(len(state.enemies)):
             if state.enemies[index].isAlive:
-                move_enemy(state, index, enemyAI(getEnemyGameState(state, index)))
+                moveEnemy(state, index, enemyAI(getEnemyGameState(state, index)))
     return state.snake.isAlive
 
 
-def _move_snake(state: GameState, snake: Snake, turn: Turn) -> bool:
-    next_head = snake.get_next_head(turn)
-    if next_head in state.walls:
+def moveSnake(state: GameState, snake: Snake, turn: Turn) -> bool:
+    nextHead = snake.get_next_head(turn)
+    if nextHead in state.walls:
         return False
-    if not (0 <= next_head[0] < state.width and 0 <= next_head[1] < state.height):
+    if not (0 <= nextHead[0] < state.width and 0 <= nextHead[1] < state.height):
         return False
-    if next_head in snake.body and not next_head == -1:
+    if nextHead in snake.body and not nextHead == -1:
         return False
-    if snake is not state.snake and next_head in state.snake.body:
+    if snake is not state.snake and nextHead in state.snake.body:
         return False
     for other_snake in state.enemies:
-        if other_snake is not snake and other_snake.isAlive and next_head in other_snake.body:
+        if other_snake is not snake and other_snake.isAlive and nextHead in other_snake.body:
             return False
-    growing = next_head in state.food
+    growing = nextHead in state.food
     snake.move(turn, grow = growing)
     if growing:
-        state.food.remove(next_head)
+        state.food.remove(nextHead)
         # if len(self.food) < self.num_food:
         #     self.spawn_food()
         snake.score += 1
@@ -90,8 +90,8 @@ def getEnemyGameState(state: GameState, enemy_index: int) -> GameState:
     )
 
 
-def move_enemy(state: GameState, enemy_index: int, turn: Turn) -> bool:
-    state.enemies[enemy_index].isAlive = _move_snake(state, state.enemies[enemy_index], turn)
+def moveEnemy(state: GameState, enemy_index: int, turn: Turn) -> bool:
+    state.enemies[enemy_index].isAlive = moveSnake(state, state.enemies[enemy_index], turn)
     if not state.enemies[enemy_index].isAlive:
         for position in state.enemies[enemy_index].body:
             state.food.add(position)
